f5d0a1e22b6ee79b85c194eb7d8dcb22ffc6a330
Furkan Şimşek <83591761+Furkan-Simsek@users.noreply.github.com>
2022-03-20 10:57:29 +0000

    Add files via upload

---
+++ index-koa.js
@@ -0,0 +1,24 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/index", (ctx) => {
+    ctx.body = "<h1>Index sayfasına hoşgeldiniz.</h1>";
+    ctx.status = 200;
+});
+router.get("/hakkimda", (ctx) => {
+    ctx.body = "<h1>Hakkımda sayfasına hoşgeldiniz.</h1>";
+    ctx.status = 200;
+});
+router.get("/iletisim", (ctx) => {
+    ctx.body = "<h1>İletişim sayfasına hoşgeldiniz.</h1>";
+    ctx.status = 200;
+});
+app.use(router.routes());
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server ${port} Portunda Çalışıyor.`);
+});
